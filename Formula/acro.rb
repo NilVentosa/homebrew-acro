83cb30952952b488d7812bf7e1c6e91209f9c156
Nil Ventosa <35202085+NilVentosa@users.noreply.github.com>
2022-12-30 08:12:46 +0100

    v0.2.0

--- Formula/acro.rb
+++ Formula/acro.rb
@@ -4,9 +4,9 @@
 class Acro < Formula
   desc "tool to query csv file of acronyms"
   homepage "https://github.com/nilventosa/acro-rs"
-  url "https://github.com/NilVentosa/acro-rs/releases/download/v0.1.0/acro-mac-apple-silicon.tar.gz"
-  sha256 "2360974bdc43896978553be8134f54bc775cc2f098a32ad4fc79061b279e7962"
-  version "0.1.0"
+  url "https://github.com/NilVentosa/acro-rs/releases/download/v0.2.0/acro-mac-apple-silicon.tar.gz"
+  sha256 "fb22fbf87717db275c0090f42141ec862575f48704ca0a510eb39add44cc8040"
+  version "v0.2.0"
 
   def install
     bin.install "acro"
